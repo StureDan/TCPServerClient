664b8b4ede8a12982e49f2b964b089c7a886014a
StureDan <std001@edu.zealand.dk>
2025-03-06 13:23:37 +0100

    Add project files.

---
+++ TCPServerClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using TCPServerClient;
+using System.Text.Json;
+
+namespace TCPEchoServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("TCP Server");
+
+            int port = 7;
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Task.Run(() => ClientHandler.HandleClient(client));
+
+            }
+            listener.Stop();
+
+
+
+
+        }
+    }
+}
